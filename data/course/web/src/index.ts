ba1b2cd38c1c51506c9d548e894e66ea2c0ad856
Mark <redwolf89@gmail.com>
2019-09-13 16:14:17 -0400

    Finished section 13

--- data/course/web/src/index.ts
+++ data/course/web/src/index.ts
@@ -1,12 +1,28 @@
 import { Collection } from './models/Collection';
 import { User, UserProps } from './models/User';
+import { UserEdit } from './views/UserEdit';
 import { UserForm } from './views/UserForm';
+import { UserList } from './views/UserList';
+
+const users = new Collection('http://localhost:3000/users', (json: UserProps) => {
+    return User.buildUser(json);
+});
+
+users.on('change', () => {
+    const root = document.getElementById('root');
+    if (root) {
+        new UserList(root, users).render();
+    }
+});
+
+users.fetch();
 
 const user = User.buildUser({name: 'Name', age: 20 });
 
 const root = document.getElementById('root')
 if (root) {
-    const userForm = new UserForm(root, user)
+    const userForm = new UserForm(root, user);
+    const userEdit = new UserEdit(root, user);
     userForm.render();
 }
 else {
--- data/course/web/src/views/UserForm.ts
+++ data/course/web/src/views/UserForm.ts
@@ -1,12 +1,13 @@
-import { User } from "../models/User";
+import { User, UserProps } from "../models/User";
 import { View } from "./View";
-export class UserForm extends View {
+export class UserForm extends View<User, UserProps> {
 
 
     eventsMap(): {[key: string]: () => void }{
         return {
             'click:.set-age': this.onSetAgeClick,
-            'click:.set-name': this.onSetNameClick
+            'click:.set-name': this.onSetNameClick,
+            'click:.save-model': this.onSaveClick
         };
     }
 
@@ -22,15 +23,17 @@ export class UserForm extends View {
         }
     }
 
+    onSaveClick = (): void => {
+        this.model.save();
+    }
+
     template(): string {
         return  `
         <div>
-            <h1>User Form</h1>
-            <div>User name: ${this.model.get("name")}</div>
-            <div>User age: ${this.model.get("age")}</div>
-            <input/>
+            <input placeholder="${this.model.get('name')}"/>
             <button class="set-name">Change Name</button>
             <button class="set-age">Set Random Age</button>
+            <button class="save-model">Save User</button>
         </div>
         `;
     }
